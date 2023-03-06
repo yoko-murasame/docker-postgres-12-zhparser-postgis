6a6f70a23c3fe90789362d42e9e3dd1299d64cc0
Mr.Shao <51045765+yoko-murasame@users.noreply.github.com>
2023-03-06 14:48:19 +0800

    add full lexical category

--- install_extension.sql
+++ install_extension.sql
@@ -1,7 +1,8 @@
 -- Install zhparser
 CREATE EXTENSION zhparser;
 CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
-ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple;
+-- add full lexical category
+ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z WITH simple;
 
 --- Install pg_stat_statements
 CREATE EXTENSION pg_stat_statements;
